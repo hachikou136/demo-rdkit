1fd30632024b8c6bb3a502363f6311ab6ebdc959
hachikou136 <118584544+hachikou136@users.noreply.github.com>
2023-02-15 11:00:06 +0000

    Add feature outputting the mol image.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from rdkit.Chem.Draw import rdMolDraw2D
 from IPython.display import SVG
 import pubchempy as pcp
 
-benzene = pcp.get_compounds('benzene', 'name')
+benzene = pcp.get_compounds('acetic acid', 'name')
+# benzene = pcp.get_compounds('benzene', 'name')
 if len(benzene) == 1: benzene = benzene[0]
 smiles = benzene.canonical_smiles
 print(smiles)  # 'C1=CC=CC=C1'
@@ -12,4 +13,5 @@ mol_ben = Chem.MolFromSmiles(smiles)
 print(type(mol_ben))  #
 print(Chem.MolToMolBlock(mol_ben))
 
-#Draw.MolToImage(mol_ben)
+#Draw.MolToImage(mol_ben) # doesn't work in replit
+Draw.MolToFile(mol_ben, 'output.svg')
